84f84eb73a72729543d98310fb96b9ef887bb575
Alexey T <rostovboy@gmail.com>
2020-01-06 17:26:49 +0300

    Added webpack.mix.js

---
+++ theme.js
@@ -0,0 +1,9 @@
+window.$ = window.jQuery = require('jquery');
+require('popper.js');
+require('bootstrap');
+require('@fancyapps/fancybox');
+
+$(() => { // Shorthand for $( document ).ready()
+"use strict";
+  //require('./partials/form/button-add-to-cart/button-add-to-cart');
+});
